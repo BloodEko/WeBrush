a80710caaf102552f3d8c8774e3cab4ebbb2e018
BloodEko <1sicherheit@gmx.de>
2020-02-11 13:51:04 +0100

    refactor OverlayBrush

--- src/de/webrush/brush/voxelsniper/OverlayBrush.java
+++ src/de/webrush/brush/voxelsniper/OverlayBrush.java
@@ -8,15 +8,12 @@ import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.world.block.BlockState;
 import com.sk89q.worldedit.world.block.BlockType;
 import com.sk89q.worldedit.world.block.BlockTypes;
-import com.sk89q.worldedit.world.registry.BlockMaterial;
 
 /**
  * Replaces existing blocks by a specific depth. <br>
  * Similar to /b over d3 from VoxelSniper. <br>
  * <br>
  * Specify natural to overlay with dirt and grass simultaneously.
- * <br>
- * Todo: refactor
  */
 public class OverlayBrush implements Brush {
 
@@ -34,49 +31,42 @@ public class OverlayBrush implements Brush {
     public void build(EditSession session, BlockVector3 pos, Pattern pattern, double size)
             throws MaxChangedBlocksException {
         
-        final int brushSize = (int) size;
-        final double brushSizeSquared = Math.pow(brushSize + 0.5, 2);
-
-
-        for (int z = brushSize; z >= -brushSize; z--)
-        {
-            for (int x = brushSize; x >= -brushSize; x--)
-            {
-                // check if column is valid
-                // column is valid if it has no solid block right above the clicked layer
-                final BlockType materialId = session.getBlock(BlockVector3.at(pos.getX() + x, pos.getY() + 1, pos.getZ() + z)).getBlockType();
-                if (isIgnoredBlock(materialId))
-                {
-                    if ((Math.pow(x, 2) + Math.pow(z, 2)) <= brushSizeSquared)
-                    {
-                        for (int y = pos.getY(); y > 0; y--)
-                        {
-                            // check for surface
-                            final BlockType layerBlockId = session.getBlock(BlockVector3.at(pos.getX() + x, y, pos.getZ() + z)).getBlockType();
-                            if (!isIgnoredBlock(layerBlockId))
-                            {
-                                for (int currentDepth = y; y - currentDepth < depth; currentDepth--)
-                                {
-                                    if (natural && y == currentDepth) { //top layer
-                                        session.setBlock(BlockVector3.at(pos.getX() + x, normalizeY(currentDepth), pos.getZ() + z), grassblock);
-                                        continue;
-                                    }
-                                    final BlockType currentBlockId = session.getBlock(BlockVector3.at(pos.getX() + x, currentDepth, pos.getZ() + z)).getBlockType();
-                                    if (isOverrideableMaterial(currentBlockId))
-                                    {
-                                        session.setBlock(BlockVector3.at(pos.getX() + x, normalizeY(currentDepth), pos.getZ() + z), pattern);
-                                    }
-                                }
-                                break;
-                            }
+        final int    brushSize   = (int) size;
+        final double brushSizeSq = Math.pow(brushSize + 0.5, 2);
+        
+        for (int z = brushSize; z >= -brushSize; z--) {
+            for (int x = brushSize; x >= -brushSize; x--) {
+                
+                BlockType materialId = session.getBlock(BlockVector3.at(pos.getX() + x, pos.getY() + 1, pos.getZ() + z)).getBlockType();
+                if (isSolid(materialId)) { // check if column is valid
+                    continue;              // column is valid if it has no solid block right above the clicked layer
+                }
+                if ((Math.pow(x, 2) + Math.pow(z, 2)) > brushSizeSq) {
+                    continue;
+                }
+                
+                for (int y = pos.getY(); y > 0; y--) {
+                    BlockType layerBlockId = session.getBlock(BlockVector3.at(pos.getX() + x, y, pos.getZ() + z)).getBlockType();
+                    if (isUnsolid(layerBlockId)) { // check for surface
+                        continue;
+                    }
+                    
+                    for (int currentDepth = y; y - currentDepth < depth; currentDepth--) {
+                        BlockVector3 currentVec = BlockVector3.at(pos.getX() + x, normalizeY(currentDepth), pos.getZ() + z);
+                        if (natural && y == currentDepth) { //top layer
+                            session.setBlock(currentVec, grassblock);
+                            continue;
+                        }
+                        BlockType currentBlockId = session.getBlock(currentVec).getBlockType();
+                        if (isSolid(currentBlockId)) {
+                            session.setBlock(currentVec, pattern);
                         }
                     }
+                    break;
                 }
             }
         }
-        
-
-        
+          
     }
     
     private int normalizeY(int y) {
@@ -89,12 +79,11 @@ public class OverlayBrush implements Brush {
         return y;
     }
     
-    private boolean isIgnoredBlock(BlockType type) {
-        BlockMaterial mat = type.getMaterial();
-        return !mat.isSolid();
+    private boolean isUnsolid(BlockType type) {
+        return !type.getMaterial().isSolid();
     }
     
-    private boolean isOverrideableMaterial(BlockType type) {
+    private boolean isSolid(BlockType type) {
         return type.getMaterial().isSolid();
     }
 
