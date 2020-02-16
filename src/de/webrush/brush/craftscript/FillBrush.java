69e04fd9f8a3559b2356b71a580640322d3d2bd9
BloodEko <1sicherheit@gmx.de>
2020-02-16 07:57:38 +0100

    add ChangeTrackr to FillBrush

--- src/de/webrush/brush/craftscript/FillBrush.java
+++ src/de/webrush/brush/craftscript/FillBrush.java
@@ -1,7 +1,7 @@
 package de.webrush.brush.craftscript;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.sk89q.worldedit.EditSession;
@@ -11,8 +11,8 @@ import com.sk89q.worldedit.function.pattern.Pattern;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.world.block.BlockState;
 import com.sk89q.worldedit.world.block.BlockType;
-import com.sk89q.worldedit.world.block.BlockTypes;
 
+import de.webrush.ChangeTracker;
 import de.webrush.ShapeCycler;
 import de.webrush.ShapeCycler.BrushFunction;
 
@@ -24,7 +24,6 @@ import de.webrush.ShapeCycler.BrushFunction;
 public class FillBrush implements Brush {
     
     private final int maxFaces;
-    @SuppressWarnings("unused")
     private final int iterations;
     
     public FillBrush(int maxFaces, int iterations) {
@@ -41,85 +40,71 @@ public class FillBrush implements Brush {
     
     private void makeFill(EditSession session, BlockVector3 click, double size) throws MaxChangedBlocksException {
         
-        // IS INT & BLOCKSTATE
-        ArrayList<Object>     blocks    = new ArrayList<>();
-        ArrayList<BlockType>  blackList = new ArrayList<>();
-        blackList.add(BlockTypes.AIR);
-        blackList.add(BlockTypes.WATER);
-        blackList.add(BlockTypes.LAVA);
+        ChangeTracker   tracker    = new ChangeTracker(session);
+        List<BlockType> blackList  = ErodeBrush.blackList;
+        BlockState[]    blockFaces = new BlockState[6];
         
         BrushFunction erode = vec -> {
-            
-            BlockState curBlockId = session.getBlock(vec);
-            if (!blackList.contains(curBlockId.getBlockType())) {
+            if (!blackList.contains(tracker.get(vec).getBlockType())) {
                 return;
             }
             
-            int          blockCnt   = 0;
-            BlockState[] blockFaces = new BlockState[6];
-            MaxFace      maxFace    = new MaxFace();
-
-            blockFaces[0] = session.getBlock(vec.add(1,0,0));
-            blockFaces[1] = session.getBlock(vec.add(-1,0,0));
-            blockFaces[2] = session.getBlock(vec.add(0,0,1));
-            blockFaces[3] = session.getBlock(vec.add(0,0,-1));
-            blockFaces[4] = session.getBlock(vec.add(0,1,0));
-            blockFaces[5] = session.getBlock(vec.add(0,-1,0));
-            
             Map<BlockType, Counter> faces = new HashMap<BlockType, Counter>();
+            int blockCnt = 0;
+
+            blockFaces[0] = tracker.get(vec.add(1,0,0));
+            blockFaces[1] = tracker.get(vec.add(-1,0,0));
+            blockFaces[2] = tracker.get(vec.add(0,0,1));
+            blockFaces[3] = tracker.get(vec.add(0,0,-1));
+            blockFaces[4] = tracker.get(vec.add(0,1,0));
+            blockFaces[5] = tracker.get(vec.add(0,-1,0));
+            
+            BlockState maxFaceBlock = null;
+            int        maxFaceCnt   = 0;
             
             for (int i = 0; i < blockFaces.length; i++) {
-
-                if (!blackList.contains(blockFaces[i].getBlockType())) {
-                    if (!faces.containsKey(blockFaces[i].getBlockType())) {
-                        faces.put(blockFaces[i].getBlockType(), new Counter());
-                    }
-                    else {
-                        Counter counter = faces.get(blockFaces[i].getBlockType());
-                        counter.cnt++;
-                    }
-                    Counter counter = faces.get(blockFaces[i].getBlockType());
-                    int cnt = counter.cnt;
-                    if (cnt > maxFace.cnt) {
-                        maxFace.block = blockFaces[i];
-                        maxFace.cnt   = cnt;
-                    }
-                    blockCnt++;
+                
+                BlockState face = blockFaces[i];
+                BlockType  type = face.getBlockType();
+                
+                if (blackList.contains(type)) {
+                    continue;
                 }
+                
+                Counter counter = faces.get(type);
+                if (counter == null) {
+                    counter = new Counter();
+                    faces.put(type, counter);
+                }
+                int cnt = counter.increment();
+                
+                if (cnt > maxFaceCnt) {
+                    maxFaceBlock = face;
+                    maxFaceCnt   = cnt;
+                }
+                blockCnt++;
             }
             
-
             if (blockCnt >= maxFaces) {
-                blocks.add(vec.getX());
-                blocks.add(vec.getY());
-                blocks.add(vec.getZ());
-                blocks.add(maxFace.block);
+                tracker.setSoft(vec, maxFaceBlock);
             }
         };
         
-        new ShapeCycler(erode, size).run(click);
-        
-        for (int i = 0; i < blocks.size(); i += 4) {
-            int     valx    = (int) blocks.get(i);
-            int     valy    = (int) blocks.get(i+1);
-            int     valz    = (int) blocks.get(i+2);
-            BlockState valType = (BlockState) blocks.get(i+3);
-            session.setBlock(BlockVector3.at(valx, valy, valz), valType);
+        for (int i = 0; i < iterations; i++) {
+            new ShapeCycler(erode, size).run(click);
+            tracker.flushSoft();
         }
-        
+        tracker.writeToSession();
     }
+    
     
     private static class Counter {
-        int       cnt;
+        private int val;
         
-        public Counter() {
-            cnt   = 1;
+        public int increment() {
+            val++;
+            return val;
         }
-    }
-    
-    private class MaxFace {
-        BlockState block;
-        int        cnt;
     }
 }
 
